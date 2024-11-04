2d7ed60b2b94ed2e660458d25ad1e19962f8c60e
Thanhdat1011 <MAY 06@LAB2-35>
2024-11-04 14:34:34 +0700

    Add project files.

---
+++ Bai2_Cong2so.cpp
@@ -0,0 +1,10 @@
+       // Bai2_Cong2so.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << " CHUONG TRINH TINH TONG HAI SO " << endl;
+    
+}      
